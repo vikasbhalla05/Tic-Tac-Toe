8e6a06bfbd12d53c887db5ecc4749a1bd8a5af8d
vikasbhalla05 <77003706+vikasbhalla05@users.noreply.github.com>
2021-06-18 20:12:01 +0530

    Clear and reset still not working

--- script.js
+++ script.js
@@ -21,82 +21,86 @@ function calculateWinner(squares){
     ];
     
     for(let i=0; i<winning_lines.length;i++){
-        const [a, b, c]=winning_lines[i]
+        const [a, b, c]=winning_lines[i];
         if(squares[a] && squares[a]===squares[b] && squares[b]===squares[c]){
-            showWinner(squares[a])
-            setTimeout(()=>resetBoard(squares[a]), 1000)
-            return square[a]
+            showWinner(squares[a]);
+            setTimeout(()=>resetBoard(squares[a]), 1000);
+            return squares[a];
         }
     }
 
-    if(!squares.include(null)){
-        showWinner(null)
-        setTimeout(()=>resetBoard(null), 1000)
-        return
+    if(!squares.includes(null)){
+        showWinner(null);
+        setTimeout(()=>resetBoard(null), 1000);
+        return;
     }
 
-    return null
+    return null;
 }
 
 function resetBoard(winner){
     if(winner){
-        winner==='X'?state.scores.player1++:state.scores.player2++
+        winner==='X'? state.scores.player1++ : state.scores.player2++ ;
     }
-    state.squares===Array.fill(null)
-    $('#player1Score').text(state.scores.player1)
-    $('#player2Score').text(state.scores.player2)
-    renderBoard()
+    state.squares=Array[9].fill(null);
+    state.xIsNext=true;
+    $('#player1Score').text(state.scores.player1);
+    $('#player2Score').text(state.scores.player2);
+    renderBoard();
 }
 
-function showWinner(winner){
-    const alert_box =$('#alert-box')
+function showWinner(winner){    
+    const alert_box =$('#alert-box');
     if(winner){
-        winner==='X'?winner= 'Player 1':winner= 'Player 2'
-        $('alert_box').html(`${winner} Won!`)
+        winner = winner==='X'?winner= 'Player 1':winner= 'Player 2';
+        alert_box.html(`${winner} Won!`);
     }
-    $('alert_box').html(`It's a Draw`)
-    $('alert_box').slideDown()
-    setTimeout(()=>$('alert_box').slideUp(),1000)
+    else
+    {    
+        alert_box.html(`It's a Draw`);
+    }
+    alert_box.slideDown();
+    setTimeout(()=>alert_box.slideUp(),1000);
 }
 
 function renderSquare(index){
-    const val=state.squares[index] ? state.squares[index] : '&nbsp;'
-    return `<div value=${index} class='box col-lg-4 col-md-4 col-sm-4 col-xs-4' onclick='boxClick(${index})'>${val}</div>`
+    const val=state.squares[index] ? state.squares[index] : '&nbsp;' ;   
+    return `<div value=${index} class='box col-lg-4 col-md-4 col-sm-4 col-xs-4' onclick='boxClick(${index})'>${val}</div>`;
 
 }
 
 function renderBoard(){
-    let board_html=' '
+    let board_html=' ';
     for(let i=0;i<9;i++){
-        board_html+=renderSquare(i)
+        board_html+=renderSquare(i);
     }
-    $('#board').html(board_html)
-    calculateWinner(state.squares)
+    $('#board').html(board_html);
+    calculateWinner(state.squares);
 }
 
 function boxClick(index){
-    const squares=state.squares
+    const squares=state.squares;
     if(calculateWinner(squares) || squares[index]){
         return; 
     }
-    squares[index]= state.xIsNext? 'X' : '0'
-    state.squares= squares
-    state.xIsNext= !state.xIsNext
-    renderBoard()
+    squares[index]= state.xIsNext? 'X' : '0';
+    state.squares= squares;
+    state.xIsNext= !state.xIsNext;
+    renderBoard();
 }
 
 function resetGame(){
     
-    state.scores.player1='0'
-    state.scores.player2='0'
+    state.scores.player1=0;
+    state.scores.player2=0;
 
-    resetBoard(null)
+    resetBoard(null);
 }
 
 $(()=>{
-    renderBoard()
-    $('#alert_box').slideUp(.001)
-    $("#clear").on('click',()=>resetBoard(null))
-    $("#reset").on('click',()=>resetGame())
+    renderBoard();
+    $('#alert-box').slideUp(0.0001);
+    $("#clear").on('click',()=>resetBoard(null));
+    $("#reset").on('click',()=>resetGame()); 
 })
 
